8fa3b9d9f9e04264b4fbb57bca4a12969d5e65f9
mostaphaelarfaoui <mostapha0411@gmail.com>
2026-01-17 20:25:05 +0100

    change port to 8000

--- main.py
+++ main.py
@@ -173,4 +173,4 @@ def extract_info(request: VideoRequest, req: Request):
 
 if __name__ == "__main__":
     import uvicorn
-    uvicorn.run(app, host="0.0.0.0", port=7860)
+    uvicorn.run(app, host="0.0.0.0", port=8000)
